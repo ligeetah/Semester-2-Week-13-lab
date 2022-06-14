a462e9dbfd2715cd31fc0c1c02103ae07a793495
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2022-06-14 10:50:27 +0500

    task 3

--- FrameWork/GameF/Game.cs
+++ FrameWork/GameF/Game.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Drawing;
+using System.Windows.Forms;
+using FrameWork.Movement;
 namespace FrameWork.GameF
 {
-    internal class Game
+    public class Game
     {
+        private List<GameObject> gameobjects;
+        public event EventHandler onGameObjectAdded;
+        public Game()
+        {
+            gameobjects = new List<GameObject>();
+        }
+        public void AddGameObject(Image img,int top,int left,IMovement movement)
+        {
+            GameObject ob = new GameObject(img,top,left,movement);
+            gameobjects.Add(ob);
+            onGameObjectAdded?.Invoke(ob.Pb, EventArgs.Empty);
+        }
+        public void update()
+        {
+            for (int i = 0; i < gameobjects.Count; i++)
+            {
+                gameobjects[i].move();
+            }
+        }
     }
+
 }
---
+++ Space/Space.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FrameWork.GameF;
+using FrameWork.Movement;
+using Space.Properties;
+namespace Space
+{
+    public partial class Space : Form
+    {
+        Game g;
+        public Space()
+        {
+            InitializeComponent();
+        }
+
+        private void Space_Load(object sender, EventArgs e)
+        {
+            g = new Game();
+            g.onGameObjectAdded += new EventHandler(Ongameobjectadded);
+            Point boundary = new Point(this.Width,this. Height);
+
+
+            g.AddGameObject(Resources.ufoGreen, 200, 200, new HorizentalMove(10, boundary, "left"));
+        }
+
+
+
+
+        public void Ongameobjectadded(object sender,EventArgs e)
+        {
+            this.Controls.Add(sender as PictureBox);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g.update();
+        }
+    }
+}
